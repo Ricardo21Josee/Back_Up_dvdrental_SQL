a93ce58922c9f42c406f647ff7835ce6f5176920
Ricardo José Márquez García <josemarquez21garcia@gmail.com>
2025-03-31 16:31:38 -0600

    Movies with longest running

--- Dvd_Rental/Códigos de SQL de DvDRental/Peliculas_con_mayor_duracion.sql
+++ Dvd_Rental/Códigos de SQL de DvDRental/Peliculas_con_mayor_duracion.sql
@@ -1,5 +1,5 @@
-/*De la tabla "Film" retorna el titulo y la duracion de las 10 peliculas con mayor duracion
-en orden alfabetico*/
+/*De la tabla "Film" retorna el titulo y la duracion de las 10 peliculas con mayor duracion en orden alfabetico*/
+/*From the "Film" table, return the title and duration of the 10 longest films in alphabetical order*/
 
 SELECT *
 FROM film
