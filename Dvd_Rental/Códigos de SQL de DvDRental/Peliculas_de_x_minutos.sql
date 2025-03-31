ce4c32442848c786926b7516e7befacd0e414572
Ricardo José Márquez García <josemarquez21garcia@gmail.com>
2025-03-31 16:33:13 -0600

    Movies of x minutes

--- Dvd_Rental/Códigos de SQL de DvDRental/Peliculas_de_x_minutos.sql
+++ Dvd_Rental/Códigos de SQL de DvDRental/Peliculas_de_x_minutos.sql
@@ -1,6 +1,5 @@
-/*De la tabla "Film" retorna la cantidad de valores únicos en la columna "rental_rate" para aquellas 
-peliculas que duren 90 min exactos*/
-
+/*De la tabla "Film" retorna la cantidad de valores únicos en la columna "rental_rate" para aquellas peliculas que duren 90 min exactos*/
+/*From the "Film" table, return the number of unique values ​​in the "rental_rate" column for those movies that last exactly 90 minutes*/
 
 SELECT 
     title, 
