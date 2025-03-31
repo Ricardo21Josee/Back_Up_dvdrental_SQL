6e80f2c89a7349bb200851292a6b32e6ee40ffa6
Ricardo José Márquez García <josemarquez21garcia@gmail.com>
2025-03-31 16:41:45 -0600

    Return last customer payments

--- Dvd_Rental/Códigos de SQL de DvDRental/Retornar ultimos pagos de cliente.sql
+++
@@ -1,8 +0,0 @@
-/*De la tabla "Payments" retorna los ultimos 3 pagos menores o iguales a 4.99
-hechos por el cliente 322*/
-
-SELECT *
-FROM payment
-WHERE amount <= 4.99 AND customer_id = 322
-ORDER BY payment_date DESC
-LIMIT 5;
