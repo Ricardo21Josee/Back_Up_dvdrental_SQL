2e3e63afd286c4736ffb86bd8b43ce6d697a7e0f
Ricardo José Márquez García <josemarquez21garcia@gmail.com>
2025-03-31 16:48:59 -0600

    Unique payment values

--- Dvd_Rental/Códigos de SQL de DvDRental/Valores_únicos_de_pagos.sql
+++ Dvd_Rental/Códigos de SQL de DvDRental/Valores_únicos_de_pagos.sql
@@ -1,4 +1,5 @@
 /*De la tabla "Payment" retornar los valores únicos de pagos que se han realizado*/
+/*From the "Payment" table, return the unique values ​​of payments that have been made*/
 
 SELECT 
 	DISTINCT
