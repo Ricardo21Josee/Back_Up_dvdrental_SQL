33876c0477ead936d33319e6cc52858bb93bda69
Ricardo José Márquez García <josemarquez21garcia@gmail.com>
2025-03-31 16:43:45 -0600

    Return last names of people called x name

--- Dvd_Rental/Códigos de SQL de DvDRental/Retornar_apellidos_de_personas_llamadas_xxx_nombre.sql
+++ Dvd_Rental/Códigos de SQL de DvDRental/Retornar_apellidos_de_personas_llamadas_xxx_nombre.sql
@@ -1,4 +1,5 @@
 /*De la tabla "Customer" retornar el apellido de todos los que se llamen "Mary"*/
+/*From the "Customer" table, return the last name of all those named "Mary"*/
 
 SELECT 
 	first_name,
