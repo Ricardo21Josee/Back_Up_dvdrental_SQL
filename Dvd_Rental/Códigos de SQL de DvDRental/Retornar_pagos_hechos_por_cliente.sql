66e041ae54675efb3da1ec85c69319f1e7a6a24a
Ricardo José Márquez García <josemarquez21garcia@gmail.com>
2025-03-31 16:39:24 -0600

    Return payments made by customer

--- Dvd_Rental/Códigos de SQL de DvDRental/Retornar_pagos_hechos_por_cliente.sql
+++ Dvd_Rental/Códigos de SQL de DvDRental/Retornar_pagos_hechos_por_cliente.sql
@@ -1,4 +1,5 @@
 /*De la tabla "Payment" retornar el monto de todos los pagos hechos por el cliente 341*/
+/*From the "Payment" table, return the amount of all payments made by customer 341*/
 
 SELECT 
 	customer_id,
