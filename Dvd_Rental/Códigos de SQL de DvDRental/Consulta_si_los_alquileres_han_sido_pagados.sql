c01ad666d962d9f17995785416075dda8a6b400b
Ricardo José Márquez García <josemarquez21garcia@gmail.com>
2025-03-31 16:24:31 -0600

    Query whether rent has been paid

--- Dvd_Rental/Códigos de SQL de DvDRental/Consulta_si_los_alquileres_han_sido_pagados.sql
+++ Dvd_Rental/Códigos de SQL de DvDRental/Consulta_si_los_alquileres_han_sido_pagados.sql
@@ -1,4 +1,5 @@
 /*Retorna todos los alquileres junto la info del pago correpondiente, si es que existe*/
+/*Returns all rentals along with the corresponding payment information, if it exists*/
 
 SELECT 
 	*
